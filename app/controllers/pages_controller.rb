03e3904786ed655d1d75bd762e706c57314b04e4
Arthur De Villemandy <arthurdevillemandy@arthurs-mbp.home>
2016-06-13 12:06:11 +0200

    routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    render plain: "Welcome !!"
+  end
+
+  def team
+    render plain: "Meet the Constellation team"
+  end
+
+  def join_us
+    render plain: "Join the adventure and contact us !"
   end
 end
